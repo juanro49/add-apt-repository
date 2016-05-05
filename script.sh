fc2aab96051d8ca2d92b68a0b71661b613771fc6
Rodrigo <rodrigo.ec@msn.com>
2016-05-04 20:43:56 -0500

    Added files via upload
    Este script inserta el script add-apt-repository.sh a la ruta final de su inserción.

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo cp ~/add-apt-repository/add-apt-repository.sh /usr/bin/add-apt-repository
+sudo chmod o+x /usr/bin/add-apt-repository
+sudo chown root:root /usr/bin/add-apt-repository
+exit
