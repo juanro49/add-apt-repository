cbb7d7ab1b4ef3ab6cd46db540a3b48c77906271
juanro49 <juanrobertogarciasanchez@gmail.com>
2020-02-28 18:12:59 +0100

    eliminar extensión e instalador

--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo cp ~/add-apt-repository/add-apt-repository.sh /usr/bin/add-apt-repository
-sudo chmod o+x /usr/bin/add-apt-repository
-sudo chown root:root /usr/bin/add-apt-repository
-exit
